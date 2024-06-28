963c8d7eb3bba9a567c923319f09ea61fdd28a8c
Steve Phelps <79sphelps@gmail.com>
2024-06-28 11:12:52 -0700

    updating loading spinner position

--- src/components/loading.js
+++ src/components/loading.js
@@ -2,7 +2,17 @@ import React from "react";
 import loading from "../assets/loading.svg";
 
 const Loading = () => (
-  <div className="spinner" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+  // <div className="spinner" style={{ 
+  <div style={{ 
+    /* High z-index so it is on top of the page */
+    display: "block",
+    position: "fixed",
+    zIndex: "1031", 
+    top: "50%",
+    right: "50%", /* or: left: 50%; */
+    marginTop: "-..px", /* half of the elements height */
+    marginRight: "-..px" /* half of the elements width */
+    }}>
     <img src={loading} alt="Loading" />
   </div>
 );
