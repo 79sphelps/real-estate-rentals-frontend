782862ba1693b9e9f840b488e060e8c877021f4a
Steve Phelps <79sphelps@gmail.com>
2024-07-02 05:55:19 -0700

    updating spinner css

--- src/components/loading.js
+++ src/components/loading.js
@@ -2,7 +2,6 @@ import React from "react";
 import loading from "../assets/loading.svg";
 
 const Loading = () => (
-  // <div className="spinner" style={{ 
   <div style={{ 
     /* High z-index so it is on top of the page */
     display: "block",
@@ -11,7 +10,7 @@ const Loading = () => (
     top: "50%",
     right: "50%", /* or: left: 50%; */
     marginTop: "-..px", /* half of the elements height */
-    marginRight: "-..px" /* half of the elements width */
+    marginRight: "-..px", /* half of the elements width */
     }}>
     <img src={loading} alt="Loading" />
   </div>
