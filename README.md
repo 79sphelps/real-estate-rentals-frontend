25b5d735fba06c23daab8d509b631cef49eabd24
Steve Phelps <79sphelps@gmail.com>
2024-07-11 20:59:31 -0700

    updating README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Rea Estate Website
 
-This is a website build using React and redux for state management. 
+This is the front end of a real estate broker website built using React and Redux for state management. It is still a bit of a WIP with lots of room to grow. I've tried to keep it simple and focus on functionality and design.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import { 
-  Route, 
-  Routes 
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import {
   Home,
   // Agents,
@@ -23,7 +20,7 @@ import {
   // AgentListing,
 } from "./pages";
 import { CallbackPage } from "./pages/callback-page";
-import Loading from './components/loading';
+import Loading from "./components/loading";
 // import { Profile } from './components/profile';
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -40,26 +37,34 @@ const App = () => {
 
   return (
     <Routes>
-        {/* <Route exact path="/" component={isAuthenticated ? CallbackPage : Home} /> */}
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="/listings" element={<Listings />} />
-        {/* <Route exact path="/listing" component={ isAuthenticated ? Listings : Home} />  */}
-        <Route exact path="/addlisting" element={ isAuthenticated ? <AddListing /> : <Home />} />
-        <Route exact path="/rentals/:id" element={<Listing />} />
-        <Route exact path="/rentals/delete/:id" element={isAuthenticated ? <DeleteListing /> : <Home />} />
-        {/* <Route exact path="/login" component={Login} /> */}
-        {/* <Route exact path="/signup" component={Signup} /> */}
-        {/* <Route exact path="/forgot-password" component={Forgot} /> */}
-        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
-        {/* <Route exact path="/profile" component={UserProfile} /> */}
-        {/* <Route exact path="/messages" component={Messages} /> */}
-        {/* <Route exact path="/change-password" component={Password} /> */}
-        {/* <Route path="/add-listing/:id?" component={AddLisiting} /> */}
-        {/* <Route exact path="/all-listing" component={AdminListingList} /> */}
-        {/* <Route exact path="/all-agents" component={AdminAgentsList} /> */}
-        {/* <Route exact path="/mylisting" component={AgentListing} /> */}
-        <Route path="/callback" element={<CallbackPage />} />
-        </Routes>
+      {/* <Route exact path="/" component={isAuthenticated ? CallbackPage : Home} /> */}
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/listings" element={<Listings />} />
+      {/* <Route exact path="/listing" component={ isAuthenticated ? Listings : Home} />  */}
+      <Route
+        exact
+        path="/addlisting"
+        element={isAuthenticated ? <AddListing /> : <Home />}
+      />
+      <Route exact path="/rentals/:id" element={<Listing />} />
+      <Route
+        exact
+        path="/rentals/delete/:id"
+        element={isAuthenticated ? <DeleteListing /> : <Home />}
+      />
+      {/* <Route exact path="/login" component={Login} /> */}
+      {/* <Route exact path="/signup" component={Signup} /> */}
+      {/* <Route exact path="/forgot-password" component={Forgot} /> */}
+      {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+      {/* <Route exact path="/profile" component={UserProfile} /> */}
+      {/* <Route exact path="/messages" component={Messages} /> */}
+      {/* <Route exact path="/change-password" component={Password} /> */}
+      {/* <Route path="/add-listing/:id?" component={AddLisiting} /> */}
+      {/* <Route exact path="/all-listing" component={AdminListingList} /> */}
+      {/* <Route exact path="/all-agents" component={AdminAgentsList} /> */}
+      {/* <Route exact path="/mylisting" component={AgentListing} /> */}
+      <Route path="/callback" element={<CallbackPage />} />
+    </Routes>
   );
 };
 
